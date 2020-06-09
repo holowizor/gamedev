6d396a976eeb0f467108a639ccdc13687ef57d0d
m-kwiecien-hrs <marek.kwiecien@innovation.hrs.com>
2020-06-09 08:44:07 +0200

    Initial import

---
+++ desktop/src/com/devbuild/gamedev/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.devbuild.gamedev.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.devbuild.gamedev.GameDev
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(GameDev(), config)
+    }
+}
